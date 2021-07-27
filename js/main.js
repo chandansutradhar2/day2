f3808823f8c9a793de3d8b20bda421db446ba508
chandan naresh <chandansutradhar2@gmail.com>
2021-07-27 14:05:47 +0530

    const & functions

--- js/main.js
+++ js/main.js
@@ -50,24 +50,70 @@ function arrayWork() {
 
   console.log(fruits, idx);
 
-  let students = [
+
+  const students =new Array( 
     { name: "chandan", score: 70 },
     { name: "samaya", score: 40 },
     { name: "ranjan", score: 50 },
-  ];
+  );
 
   //sorting on objects with number
   //   students.sort((a, b) => {
   //     return a.score - b.score;
   //   });
-
+    //adding new item to array
+    //in js we declare object with parenthesis
+    students.push({name:'harsha',score:87});
+    
   //sorting on object with string
-  students.sort((a, b) => {
-    return a.name > b.name ? 1 : -1;
-  });
+  // students.sort((a, b) => {
+  //   return a.name > b.name ? 1 : -1;
+  // });
 
   console.log(students);
 }
+
+function constDemo() {
+//  'use strict'
+  // const students = ["chandan", "ranjan", "samaya"];
+  // students.push("harsha");
+  // console.log(students);
+  // students = new Array("vinod", "aishwarya");
+
+  //use case:
+  //when we have static data which shouldnt change during runtime
+ const apiEndpoint =
+    "api.openweathermap.org/data/2.5/weather?";
+  const apiKey = "6ed8cf0cef974d3e2246675ff6340b96";
+
+  //TODO add axios library to fetch weather api
+ // apiKey = "some new value";
+  
+  
+}
+
+//function example - part 1: declaration
+//function overloading not supported OOB in javascript
+//always define argument with name while declaring function
+function generateCar(color) {
+  console.log('generateCar function invoked with car color',color);
+}
+
+//function with self invocation
+function showTimer() {
+  let cnt = 0;
+  setInterval(() => {
+    cnt++;
+    console.log('you have been logged in since ' + cnt * 10 + 'seconds');
+    console.log(`you have been logged in since ${cnt * 10} seconds`);
+    document.getElementById('msg').innerHTML = cnt * 10;
+  }, 10000);
+}
+
+//part 2: invocation
+generateCar('red'); //pass parameter while invocation.
+showTimer();
 //dateType();
-arrayWork();
+//arrayWork();
+//constDemo();
 //checkOperator();
